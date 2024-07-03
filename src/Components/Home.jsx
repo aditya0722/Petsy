e9d58ae74874175cdab8f074fe30b4babc120024
Aditya sharma <sharmaaaditya167@gmail.com>
2024-07-03 08:41:58 +0530

    home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { isLoggedIn,getCookie } from "../cookie";
+import img from "../img/tspe.png";
 function Home({ onLogin }) {
   const history = useNavigate();
 
@@ -12,10 +13,25 @@ function Home({ onLogin }) {
   }, []); 
 
   return (
-    <div>
-      <h1> Home page content </h1>
+    <>
+     <section className="home-container">
+        <div className="home">
+            <div className="home-txt">
+                <span className="txt-home">Wanted to Sell/Buy pet?</span>
+                <h4 className="txt-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab quasi consequaturdolor culpa architecto labore asperiores debitis ratione vel vitae!</h4>
+                <br/>
+                <button type="button" className="btn-buy-sell ">Buy&Sell</button>
+            </div>
+            <div className="img-home">
+                <div className="img-box">
+                    <img src={img} alt="img" height="90%" width="100%"/>
+                </div>
+            </div>
+
+        </div>
+    </section>
      
-    </div>
+    </>
   );
 }
 
--- src/mongo.js
+++ src/mongo.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/petsy", {
+mongoose.connect("mongodb+srv://sharmaaaditya167:oT4SzT9CZTkvhtYa@users.lofs8gm.mongodb.net/users?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  ssl: true, // Ensure SSL is enabled
+  tlsAllowInvalidCertificates: false, // Set to false for production
+  tlsAllowInvalidHostnames: false, // Set to false for production
   
 })
   .then(() => {
